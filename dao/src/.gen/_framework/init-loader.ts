6901318284cccb2a826eb1598d8025dc94a2aaab
thounyy <thouny@tuta.io>
2025-04-25 14:53:49 +0200

    chore: update dao sui-client-gen

--- dao/src/.gen/_framework/init-loader.ts
+++ dao/src/.gen/_framework/init-loader.ts
@@ -1,13 +1,13 @@
 import * as package_source_1 from "../_dependencies/source/0x1/init";
 import * as package_source_2 from "../_dependencies/source/0x2/init";
-import * as package_source_60bdbac4243b28773ca6d354fe5da75372552838d3fe766577bb48560600f27f from "../_dependencies/source/0x60bdbac4243b28773ca6d354fe5da75372552838d3fe766577bb48560600f27f/init";
-import * as package_source_58438e1f3ecdaee65fc2076647bf04ac173bc4bb1335ee64027a17cf9f0bf5a7 from "../account-protocol/init";
+import * as package_source_c60d0aa043cadd8ea6b905d46fee76bedb6389052632b31e973e4a0f7e83d643 from "../_dependencies/source/0xc60d0aa043cadd8ea6b905d46fee76bedb6389052632b31e973e4a0f7e83d643/init";
+import * as package_source_de3063e4616e600224b93d213620d3fb5d2abeda0101685c2f7c8b7d08da8143 from "../account-protocol/init";
 import {StructClassLoader} from "./loader";
 
 function registerClassesSource(loader: StructClassLoader) { package_source_1.registerClasses(loader);
 package_source_2.registerClasses(loader);
-package_source_58438e1f3ecdaee65fc2076647bf04ac173bc4bb1335ee64027a17cf9f0bf5a7.registerClasses(loader);
-package_source_60bdbac4243b28773ca6d354fe5da75372552838d3fe766577bb48560600f27f.registerClasses(loader);
+package_source_c60d0aa043cadd8ea6b905d46fee76bedb6389052632b31e973e4a0f7e83d643.registerClasses(loader);
+package_source_de3063e4616e600224b93d213620d3fb5d2abeda0101685c2f7c8b7d08da8143.registerClasses(loader);
  }
 
 export function registerClasses(loader: StructClassLoader) { registerClassesSource(loader); }
--- dao/src/lib/constants.ts
+++ dao/src/lib/constants.ts
@@ -1,9 +1,9 @@
 export enum ACCOUNT_DAO {
-    V1 = "0x23f488a687766b21ac2ab80533d435fb83e7b3bb2f64e4536aae7497b28e61aa",
+    V1 = "0x31a2fdac3b7d9458b0779a95bed1ee4cb4845099f227685416995f58a2e13450",
 }
 // Types
 export const DAO_CONFIG_TYPE = `${ACCOUNT_DAO.V1}::dao::Dao`;
 export const DAO_GENERICS: [string, string] = [`${ACCOUNT_DAO.V1}::dao::Dao`, `${ACCOUNT_DAO.V1}::dao::Votes`];
 // Shared objects
-export const DAO_REGISTRY = "0xd314532377133327531c822d6302788b8e3a760f7405d590f24a57442f41d8ca";
+export const DAO_REGISTRY = "0x643fcab19e79f9d02dbf1c9c45e12235f430c91b7d09aac13f7f8a98f902cefe";
 export const DAO_FEES = ""; // none for now
